2f1112e550b18f46a912c4cf9c28e293e99f134c
stalexsm <stalex.info@yandex.ru>
2023-08-22 22:56:55 +0300

    feat: Socket сделал многопоточным

--- smarthouse/examples/socket.rs
+++ smarthouse/examples/socket.rs
@@ -1,6 +1,8 @@
 use std::{
     io::{Read, Write},
     net::TcpListener,
+    sync::{Arc, RwLock},
+    thread,
 };
 
 use smarthouse::devices::SmartSocket;
@@ -12,7 +14,9 @@ fn main() {
     let server_address = args.next().unwrap_or_else(|| "127.0.0.1:7890".into());
 
     let listener = TcpListener::bind(server_address).expect("can't bind tcp listener");
-    let mut smart_socket = SmartSocket::new(String::from("Aqara Socket H1"));
+    let smart_socket = Arc::new(RwLock::new(SmartSocket::new(String::from(
+        "Aqara Socket H1",
+    ))));
 
     while let Some(connection) = listener.incoming().next() {
         let mut stream = match connection {
@@ -29,14 +33,19 @@ fn main() {
             .unwrap_or_else(|_| "unknown".into());
         println!("Peer '{peer}' connected");
 
-        let mut in_buffer = [0u8];
-        while stream.read_exact(&mut in_buffer).is_ok() {
-            let response = smart_socket.process_command(in_buffer[0].into());
-            let response_buf: [u8; 5] = response.into();
-            if stream.write_all(&response_buf).is_err() {
-                break;
-            };
-        }
+        let smart_socket = smart_socket.clone();
+        thread::spawn(move || {
+            let mut in_buffer = [0u8];
+            while stream.read_exact(&mut in_buffer).is_ok() {
+                let mut smart_socket = smart_socket.write().unwrap();
+
+                let response = smart_socket.process_command(in_buffer[0].into());
+                let response_buf: [u8; 5] = response.into();
+                if stream.write_all(&response_buf).is_err() {
+                    break;
+                };
+            }
+        });
 
         println!("Connection with {peer} lost. Waiting for new connections...");
     }
