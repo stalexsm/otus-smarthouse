776f8be8d7e8a3481ac82cab9aa3599dbdb8e6d5
stalexsm <stalex.info@yandex.ru>
2023-09-25 16:45:36 +0300

    fix: Обработка в example ошибки thiserror

--- thermo_lib/examples/use_thermo.rs
+++ thermo_lib/examples/use_thermo.rs
@@ -4,7 +4,14 @@ use thermo_lib::SmartThermo;
 
 fn main() {
     let receiver_addrs = "127.0.0.1:43211";
-    let thermo = SmartThermo::new(receiver_addrs).unwrap();
+
+    let thermo = match SmartThermo::new(receiver_addrs) {
+        Ok(thermo) => thermo,
+        Err(thermo_lib::SmartThermoError::Io(e)) => {
+            println!("{}", e);
+            return;
+        }
+    };
 
     for _ in 0..120 {
         thread::sleep(Duration::from_secs(1));
